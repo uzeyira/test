91b9154d51212ac41e333c33d68d7dc3dd8a398d
uzeyira <melkbij@gmail.com>
2025-12-08 22:10:04 +0100

    edit

--- src/sum.py
+++ src/sum.py
@@ -47,7 +47,7 @@ def secim():
     print(" Hangi seviyeden başlamak istiyorsunuz (çıkış = -1) ?\n")
     print("  1 - Kolay ")
     print("  2 - Orta ")
-    print("  3 - Zor")
+    print("  3 - Zordur")
     print("  4 - Çok zor\n")
 
     svy = input(" >> ")
